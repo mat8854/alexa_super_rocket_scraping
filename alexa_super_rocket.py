e52262edb1833c49b3ff0ff1b5a53b70dfa07ddd
Matthias Dittmer <mail@matthiasdittmer.de>
2019-01-08 15:48:26 +0100

    Fixing typos and ununsed statement.

--- alexa_super_rocket.py
+++ alexa_super_rocket.py
@@ -10,14 +10,13 @@ USE_GOOGLE_TRANSLATION_API_FOR_FREE = True
 OUTPUT_FILE = "data.html"
 url = "https://spaceflightnow.com/launch-schedule/"
 
-def scrape_site_and__write_output():
+def scrape_site_and_write_output():
 
     http = urllib3.PoolManager(timeout=1.0)
     r = http.request('GET', url)
     data = r.data
 
     soup = BeautifulSoup(data, "html.parser")
-    soup.find()
 
     when = soup.find("span", {"class": "launchdate"}).contents[0].strip()
     rocket = soup.find("span", {"class": "mission"}).contents[0].split("•")[0].strip()
@@ -29,14 +28,14 @@ def scrape_site_and__write_output():
     # remove trailing "[" if present
     if information[-1:] == "[":
         information = information[:-1].strip()
-    information_de = tranlate_en_to_de(information).strip()
-    when_de = tranlate_en_to_de(when).strip()
+    information_de = translate_en_to_de(information).strip()
+    when_de = translate_en_to_de(when).strip()
 
     # write a plain string, separator is "|"
     with open(OUTPUT_FILE, 'w') as file:
         file.write(when + "|" + rocket + "|" + payload + "|" + launch_window + "|" + where + "|" + information + "|" + information_de + "|" + when_de)
 
-def tranlate_en_to_de(str):
+def translate_en_to_de(str):
 
     if USE_GOOGLE_TRANSLATION_API_FOR_FREE:
 
@@ -83,4 +82,4 @@ if __name__ == "__main__":
         # point to credentials JSON (placed outside of www folder, here in bitnami environment)
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/bitnami/google.json"
 
-    scrape_site_and__write_output()
+    scrape_site_and_write_output()
